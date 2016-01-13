92a41ded94bbdc1faa7275af0015c255da040f24
miom <lex.antonov@gmail.com>
2016-01-13 06:40:24 +0300

    functions

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
 (defproject moslenta_dispatcher "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "dispatcher and analyse text"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
                  [com.novemberain/monger "3.0.0-rc2"]
-                 [org.clojure/core.async "0.2.374"]]
+                 [org.clojure/core.async "0.2.374"]
+                 [judgr "0.3.0"]
+                 [judgr/mongodb "0.1.3"]]
   :main moslenta-dispatcher.core)
--- src/moslenta_dispatcher/analyse.clj
+++ src/moslenta_dispatcher/analyse.clj
@@ -1,1 +1,31 @@
-(ns moslenta-dispatcher.analyse)
+(ns moslenta-dispatcher.analyse
+  (:require [judgr.core]
+            [clojure.core.string :as str])
+  (:use [judgr.core]
+        [judgr.settings]
+        [judgr.mongo.db]))
+
+(def new-settings
+  (update-settings settings
+                   [:database :type] :mongo
+                   [:database :mongo] {:database "classifficator"
+                                       :host     "localhost"
+                                       :port     27017
+                                       :auth?    false
+                                       :username ""
+                                       :password ""}
+                   [:classifier :default] {:smoothing-factor 4}))
+
+(def classifier (classifier-from new-settings))
+
+(defn remove-trash [text]
+  (join " "
+        (remove #(< (count %1) 4)
+                (str/split text #" "))))
+
+(defn like [text user_id]
+  (.train! classifier (remove-trash text) :liked))
+
+(defn dislike [text user_id]
+  (.train! classifier (remove-trash text) :dislike))
+
