14c5ddc06eaeb9bc0a5ad657aecf5cd8873a3f2f
Antonov Alexey <a.antonov@rambler-co.ru>
2016-02-19 20:37:49 +0300

    update

--- project.clj
+++ project.clj
@@ -6,8 +6,11 @@
                  [org.clojure/data.json "0.2.6"]
                  [judgr/mongodb "0.1.3"]
                  [congomongo "0.4.7"]
+                 [org.clojure/java.jdbc "0.4.2"]
+                 [korma "0.4.2"]
                  [com.novemberain/monger "3.0.1"]
                  [org.clojure/core.async "0.2.374"]
                  [judgr "0.3.0"]
                  ]
+  :plugins [[migratus-lein "0.1.7"]]
   :main moslenta-dispatcher.core)
