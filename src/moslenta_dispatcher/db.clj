5d45286f28ef07a8ad2b3c997505bc7a89d4d13c
Antonov Alexey <a.antonov@rambler-co.ru>
2016-02-25 20:43:43 +0300

    update

--- src/moslenta_dispatcher/db.clj
+++ src/moslenta_dispatcher/db.clj
@@ -3,25 +3,12 @@
             [monger.collection :as mc])
   (:gen-class))
 
-(def conn (mg/connect))
+(defdb prod
+       (postgres { :db "articles"
+                   :user "db"
+                   :password "dbpass"}))
 
-(def db (mg/get-db conn "moslenta-dispatcher"))
+(defentity articles)
 
-(defn insert-data [data]
-  "Insert entity to topics tree"
-  (mc/insert-and-return db "topics" data))
-
-(defn find-entity
-  "Find one topic by id"
-  [id]
-  (mc/find-one db "topics" {:id id}))
-
-(defn find-user
-  "Find user by id"
-  [id]
-  (mc/find-one db "users" {:id id}))
-
-(defn find-topics-by-user-id
-  "Finder"
-  [user_id]
-  (mc/find))
+(def last-articles [count]
+  (-> select *))
